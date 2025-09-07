fb4b58bffd8d4953c804888285ce835123aae490
Barrie Seldon <barrieseldon@gmail.com>
2025-09-07 08:57:37 +0800

    chore: initial commit

---
+++ BAS-ICTPRG431-AT2-POR-SQL-Pt2.sql
@@ -0,0 +1,161 @@
+-- --------------------------------------------------------------------
+-- Filename:   XXX-ICTPRG402-Portfolio-SQL-1.txt
+-- Author:     GIVEN & FAMILY NAMES
+-- Email:      ID@tafe.wa.edu.au
+-- --------------------------------------------------------------------
+-- Purpose:   
+--    This file contains the SQL used to create and execute 
+--    the solutions for the assessment ICTPRG402 Portfolio
+-- --------------------------------------------------------------------
+-- Declaration:
+--    By completing and submitting this assessment 
+--    via the Blackboard LMS or other methods, to my 
+--    lecturer, I am stating that: 
+--      * The attached submission is completely own work 
+--      * I have correctly indicated all sources of information
+--        used in this work (if required) 
+--      * I have kept a copy of this assessment (where practicable)
+--      * I understand a copy of my assessment will be kept by
+--        NMTAFE for their records 
+--      * I understand my assessment may be selected for use in
+--        NMTAFE’s validation and audit process to ensure student 
+--        assessment is valid and meets requirements of the unit 
+--        of competency
+-- --------------------------------------------------------------------
+--
+-- Add your SQL between each heading comment as required.
+--
+-- --------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------
+-- Query 01: Write & run a SQL query to find the names of 
+--           all the employees 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 02: Write & run a SQL query to find all clients 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 03: Write & run a SQL query to find all employees ordered
+--           by salary. 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 04: Write & run a SQL query to find all employees 
+--           ordered by gender 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 05: Write & run a SQL query to find all employees
+--           ordered by family name 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 06: Write & run a SQL query to find first 5 employees 
+--           in the table 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 07: Write & run a SQL query to find employees whose name 
+--           starts with a letter ‘A’.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 08: Write & run a SQL query to sort all the 
+--           employee records in a descending order based on salary.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 09: Write & run a SQL query to find all male employees 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 10: Write & run a SQL query to find all employees at the
+--           Scranton Branch (branch 2) 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 11: Write & run a SQL query to find all employee's IDs 
+--           and names who were born after 1969 
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 12: Write & run a SQL query to find all female employees 
+--           at Scranton Branch (Branch 2)
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 13: Write & run a SQL query to find all female employees 
+--           born after 1969 or who has salary greater than 80000.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 14: Write & run a SQL query to find all employees born 
+--           between 1970 and 1975.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 15: Write & run a SQL query to find all the employees 
+--           named Jim, Michael, Johnny or David.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 16: Write & run a SQL query to find the name of a client who 
+--           has got the word ‘LLC’ in his name.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 17: Write & run a SQL query to find any branch suppliers with
+-- label in their name.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 18: Write & run a SQL query to find any employee born on the 
+--           5th day of the month.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- Query 19: Write & run a SQL query to find any clients who 
+--           has got the word ‘Law’ in their name
+-- --------------------------------------------------------------------
+
+
+
