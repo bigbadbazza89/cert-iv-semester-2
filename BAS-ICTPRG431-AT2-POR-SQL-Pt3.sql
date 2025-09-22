c6bc5a613072f6f1e982e39982417046486e8718
Barrie Seldon <barrieseldon@gmail.com>
2025-09-22 12:16:36 +0800

    chore: initial commit

---
+++ BAS-ICTPRG431-AT2-POR-SQL-Pt3.sql
@@ -0,0 +1,127 @@
+-- --------------------------------------------------------------------
+-- Filename:   XXX-ICTPRG402-Portfolio-SQL-1.txt
+-- Author:     GIVEN & FAMILY NAMES
+-- Email:      ID@tafe.wa.edu.au
+-- --------------------------------------------------------------------
+-- Purpose:
+--    This file contains the SQL used to create and execute
+--    the solutions for the assessment ICTPRG402 Portfolio
+-- --------------------------------------------------------------------
+-- Declaration:
+--    By completing and submitting this assessment
+--    via the Blackboard LMS or other methods, to my
+--    lecturer, I am stating that:
+--      * The attached submission is completely own work
+--      * I have correctly indicated all sources of information
+--        used in this work (if required)
+--      * I have kept a copy of this assessment (where practicable)
+--      * I understand a copy of my assessment will be kept by
+--        NMTAFE for their records
+--      * I understand my assessment may be selected for use in
+--        NMTAFEâ€™s validation and audit process to ensure student
+--        assessment is valid and meets requirements of the unit
+--        of competency
+-- --------------------------------------------------------------------
+--
+-- Add your SQL between each heading comment as required.
+--
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- 01	Statistical Queries 1: Total Employees
+-- Write & run an SQL query to find the total number of employees
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 02	Statistical Queries 2: Total Sales
+-- Write & run an SQL query to find the average totals sales in the working_with table
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- A03	Statistical Queries 3: Total Salaries
+-- Write & run an SQL query to find sum of salaries for all employees
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 04	Statistical Queries 4: Demographics
+-- Write & run a SQL query to find the number of employees by gender
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 05	Statistical Queries 5: Maximum Salary
+-- Write & run an SQL query to find the entire record of the employee with the maximum salary
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 06	Statistical Queries 6: Employee with Lowest Salary
+-- Write & run a SQL query to find the first name and last name of the employee with the minimum salary
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 07	Statistical Queries 7: Average Salary
+-- Write & run an SQL query to find average of salaries (use functions sum and count to get an average).
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 08	Join Queries 1: Employees and Branches
+-- Write & run an SQL query to find a list of employee names and the name of the branch they work at.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 09	Join Queries 2: Clients and Branch Suppliers
+-- Write & run an SQL query to find a list of clients and their related branch suppliers’ name.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 10	Join Queries 3: Client and Total Sales
+-- Write & run an SQL query to list all clients and associated total sales from working_with.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 11	Join Queries 4: Client and branch Supplier
+-- Write & run an SQL query to find branch ids from both the clients table and branch supplier table by using union
+--------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 12	Join Queries 5: Branch, Supplier & Supplied Products
+-- Write & run an SQL query to list the name of each branch with its related supplier name(s) and product(s) supplied.
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 13	Join Queries 6: Branches and Managers
+-- Write & run an SQL query to list the names of all branches and the names of their managers
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 14	Join Queries 7: Income and Expenditures
+-- Write & run an SQL query to list all the expenditures and incomes for the company.
+-- (Hint: Where Expenditure is Salaries and Income is Total sales)
+-- --------------------------------------------------------------------
+
+
