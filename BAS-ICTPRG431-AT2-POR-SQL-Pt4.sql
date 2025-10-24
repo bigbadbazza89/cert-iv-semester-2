5be89ed2212a591a7a56535aa16deb2f746967d8
Barrie Seldon <barrieseldon@gmail.com>
2025-10-24 17:20:37 +0800

    chore: initial commit

---
+++ BAS-ICTPRG431-AT2-POR-SQL-Pt4.sql
@@ -0,0 +1,104 @@
+-- --------------------------------------------------------------------
+-- Filename:   XXX-ICTPRG402-Portfolio-SQL-1.txt
+-- Author:     GIVEN & FAMILY NAMES
+-- Email:      ID@tafe.wa.edu.au
+-- --------------------------------------------------------------------
+-- Purpose:   
+--    This file contains the SQL used to create and execute 
+--    the solutions for the assessment ICTPRG402 Portfolio
+-- --------------------------------------------------------------------
+-- Declaration:
+--    By completing and submitting this assessment 
+--    via the Blackboard LMS or other methods, to my 
+--    lecturer, I am stating that: 
+--      * The attached submission is completely own work 
+--      * I have correctly indicated all sources of information
+--        used in this work (if required) 
+--      * I have kept a copy of this assessment (where practicable)
+--      * I understand a copy of my assessment will be kept by
+--        NMTAFE for their records 
+--      * I understand my assessment may be selected for use in
+--        NMTAFE’s validation and audit process to ensure student 
+--        assessment is valid and meets requirements of the unit 
+--        of competency
+-- --------------------------------------------------------------------
+--
+-- Add your SQL between each heading comment as required.
+--
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- 01	NESTED QUERIES 1
+-- Write & run a SQL query to find names of all employees who have sold over 50,000.
+ 
+-- --------------------------------------------------------------------
+
+
+-- --------------------------------------------------------------------
+-- 02	NESTED QUERIES 2
+-- Write & run a SQL query to find all clients who are handled by the branch
+-- managed by
+-- Michael Scott (Assume you know Michael's ID).
+-- --------------------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------
+-- 03	NESTED QUERIES 3
+-- Write & run a SQL query to find the names of employees who deals 
+-- with clients of Stamford branch.
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+
+-- 04 View 4: Create View
+-- Create a view of  branch name, all the suppliers and supply type from 
+-- tables branches and Branch_suppliers that deal with Branch=3.  
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- 05 View 5: Create View
+-- Create a View for supervisor id, name, branch name, branch manager
+-- start date, Product supplied name from employees, branches and 
+-- branch supplier tables for branch id=2. 
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- 06 View 6: Delete View
+-- Drop view that was created in query 04. (Create a View of all the suppliers and
+-- Clients from the tables Clients and Branch_Suppliers that deal with Branch=3 ).
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- Procedure 07: Create Procedure
+-- Create a procedure to browse table employees. 
+-- (Hint: - Command to use -select * from employee)
+
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- Procedure 08: Create Procedure
+-- Create a procedure to find name of employee and his salary from table 
+-- employee using parameters.
+
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- Procedure 09: Delete Procedure
+-- Drop Procedure created in Query 07.
+  
+
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- Update query 010 UPDATE TABLE STRUCTURE & DATA 
+-- Write & run a SQL query to add a column with the field name 
+-- Manager’s Name (data type varchar) in Branch Supplier Table. 
+-- Add the names of Managers in all the records of Branch Supplier 
+-- Table. 
+-- --------------------------------------------------------------------
+
+
+
+
+
